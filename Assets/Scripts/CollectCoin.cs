3e8a82f1f7b10bfc33c09099b04be31fec89b7ad
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-02-23 14:38:02 -0800

    I added Coin Collectibles and added a face for the Player Object.
    I added Coin Collectibles with a score system displayed on the screen. I also gave my Player Object a unique face

---
+++ Assets/Scripts/CollectCoin.cs
@@ -0,0 +1,32 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    public static event Action isCollected;
+    public float score = 0;
+    [SerializeField] private TextMeshProUGUI scoreText;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localRotation = Quaternion.Euler(90f,Time.time*100f,0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")){
+            isCollected?.Invoke();
+            Destroy(gameObject);
+            IncreaseScore();
+        }
+    }
+
+    private void IncreaseScore()
+    {
+        score++;
+        scoreText.text = $"Coins Collected: {score}";
+    }
+}
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(inputXYZ * speed);
     }
 
-    void OnCollisionStay(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         playerOnGround = true;
